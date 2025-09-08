f30bea5b80287c98a13259056f1ac3a4435133a5
Lubna Febin <lubnafebin13@gmail.com>
2025-09-08 22:16:15 +0530

    feat: add weather display components and adjust layout for better visibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { CiSearch } from "react-icons/ci";
+import { FaSun } from "react-icons/fa";
 
 function App() {
   return (
@@ -9,6 +10,25 @@ function App() {
           <button>
             <CiSearch />
           </button>
+        </div>
+        <FaSun className="icon" />
+        <p className="temperature">25°c</p>
+        <p className="location">London</p>
+        <div className="weather-data">
+          <div className="col">
+            <i></i>
+            <div>
+              <p>91%</p>
+              <span>Humidity</span>
+            </div>
+          </div>
+          <div className="col">
+            <i></i>
+            <div>
+              <p>3.6 km/h</p>
+              <span>Wind Speed</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
