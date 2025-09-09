2443ff0dddeb9f8c77fc8d063cf38af49dc6877f
Lubna Febin <lubnafebin13@gmail.com>
2025-09-09 16:57:39 +0530

    feat: enhance weather display layout and improve styling for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,42 @@
 import { CiSearch } from "react-icons/ci";
-import { FaSun } from "react-icons/fa";
+import { FaSun, FaWind } from "react-icons/fa";
+import { WiHumidity } from "react-icons/wi";
 
 function App() {
   return (
-    <>
-      <div className="container">
-        <div className="search-bar">
-          <input type="text" placeholder="Search" />
-          <button>
-            <CiSearch />
-          </button>
-        </div>
+    <div className="container">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <button>
+          <CiSearch />
+        </button>
+      </div>
+      <div className="weather-main">
         <FaSun className="icon" />
-        <p className="temperature">25°c</p>
-        <p className="location">London</p>
-        <div className="weather-data">
-          <div className="col">
-            <i></i>
-            <div>
-              <p>91%</p>
-              <span>Humidity</span>
-            </div>
+        <span className="temperature">25°c</span>
+        <span className="location">London</span>
+      </div>
+      <div className="weather-data">
+        <div className="col">
+          <i>
+            <WiHumidity />
+          </i>
+          <div>
+            <p>91%</p>
+            <span>Humidity</span>
           </div>
-          <div className="col">
-            <i></i>
-            <div>
-              <p>3.6 km/h</p>
-              <span>Wind Speed</span>
-            </div>
+        </div>
+        <div className="col">
+          <i>
+            <FaWind />
+          </i>
+          <div>
+            <p>3.6 km/h</p>
+            <span>Wind Speed</span>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 export default App;
