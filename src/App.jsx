075715d0ee49c1d63034a18965dd1bfa84a43583
Lubna Febin <lubnafebin13@gmail.com>
2025-09-09 20:47:02 +0530

    feat: implement Weather component with search functionality and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,6 @@
-import { CiSearch } from "react-icons/ci";
-import { FaSun, FaWind } from "react-icons/fa";
-import { WiHumidity } from "react-icons/wi";
+import { Weather } from "./components/Weather";
 
 function App() {
-  return (
-    <div className="container">
-      <div className="search-bar">
-        <input type="text" placeholder="Search" />
-        <button>
-          <CiSearch />
-        </button>
-      </div>
-      <div className="weather-main">
-        <FaSun className="icon" />
-        <span className="temperature">25°c</span>
-        <span className="location">London</span>
-      </div>
-      <div className="weather-data">
-        <div className="col">
-          <i>
-            <WiHumidity />
-          </i>
-          <div>
-            <p>91%</p>
-            <span>Humidity</span>
-          </div>
-        </div>
-        <div className="col">
-          <i>
-            <FaWind />
-          </i>
-          <div>
-            <p>3.6 km/h</p>
-            <span>Wind Speed</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <Weather />;
 }
 export default App;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,41 @@
+import { CiSearch } from "react-icons/ci";
+import { FaSun, FaWind } from "react-icons/fa";
+import { WiHumidity } from "react-icons/wi";
+import "./weather.css";
+export const Weather = () => {
+  return (
+    <div className="container">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <button>
+          <CiSearch />
+        </button>
+      </div>
+      <div className="weather-main">
+        <FaSun className="icon" />
+        <span className="temperature">25°c</span>
+        <span className="location">London</span>
+      </div>
+      <div className="weather-data">
+        <div className="col">
+          <i>
+            <WiHumidity />
+          </i>
+          <div>
+            <p>91%</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <i>
+            <FaWind />
+          </i>
+          <div>
+            <p>3.6 km/h</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
