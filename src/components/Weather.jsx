377202f6dd25e7fa24857c6fe30ef63db9d3a44e
Lubna Febin <lubnafebin13@gmail.com>
2025-09-10 21:00:43 +0530

    feat: implement weather search functionality and simplify main rendering

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,7 +2,30 @@ import { CiSearch } from "react-icons/ci";
 import { FaSun, FaWind } from "react-icons/fa";
 import { WiHumidity } from "react-icons/wi";
 import "./weather.css";
+import { useEffect } from "react";
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 export const Weather = () => {
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    search("London");
+  }, []);
+
   return (
     <div className="container">
       <div className="search-bar">
